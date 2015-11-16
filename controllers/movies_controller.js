89182d29a5541e575a83589799d95afa3b71ebe7
Kate Wood <katewood611@gmail.com>
2015-11-16 15:54:10 -0800

    adds other commands to Movie query in controller

--- controllers/movies_controller.js
+++ controllers/movies_controller.js
@@ -2,9 +2,12 @@ var Movie = require('../models/movie.js')
 
 //index action
 function index(req, res) {
-	Movie.find({}, function(err, movies) {
-		if (err) throw err
-		res.json(movies)
+	Movie.find({})
+		.limit(10)
+		.sort({year: -1})
+		.exec(function(err, movies) {
+			if (err) throw err
+			res.json(movies)
 	})
 }
 
